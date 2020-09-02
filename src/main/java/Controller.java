ff53b87f3e5ba9c15be11f0958bf5fa039cbb8d5
bsant <bsantiago431@gmail.com>
2020-09-02 19:40:37 -0400

    Updated Production Project

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,10 +1,38 @@
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
 
 public class Controller {
 
   @FXML
   private Label lblOutput;
+
+  @FXML
+  private Tab tab1;
+
+  @FXML
+  private Button btnAddProduct;
+
+  @FXML
+  private Tab tab2;
+
+  @FXML
+  private Tab tab3;
+
+  @FXML
+  void addProduct(ActionEvent event) {
+    System.out.println("Product added");
+  }
+
+  @FXML
+  void recordProduct(ActionEvent event) {
+    System.out.println("Product Recorded");
+
+  }
+
+
 
 
 
