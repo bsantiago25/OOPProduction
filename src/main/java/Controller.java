0c0938715ae558b580b872c3cdc26e2f7b54c5ec
bsant <bsantiago431@gmail.com>
2020-09-19 15:15:41 -0400

    Sprint 1 Alpha update

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,13 +1,17 @@
+//Programmer name: Brandon Santiago
+//This file is the controller.java file and contains and contains all SQL codes and
+//most of the coding for this program.
+//Date: 9/19/2020
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -38,58 +42,69 @@ public class Controller {
 
   @FXML
   void addProduct(ActionEvent event) {
-    connectToDb();//calls connectToDb method when add product button is pushed.
+    connectToDb();
+    //calls connectToDb method when add product button is pushed.
 
   }
 
   @FXML
   void recordProduct(ActionEvent event) {
-    System.out.println("Product Recorded");//prints out "product recorded" into console
+    System.out.println("Product Recorded");
+    //prints out "product recorded" into console
 
   }
 
+  /**
+   * This method initializes the combobox to make a total types of 1-10.
+   */
   public void initialize() {
-    for(int i = 1; i<=10; i++)
-    cmbType.getItems().add(String.valueOf(i));//for loop for combobox. Lists numbers 1-10
+    for (int i = 1; i <= 10; i++) {
+      cmbType.getItems().add(String.valueOf(i));
+      //for loop for combobox. Lists numbers 1-10
+    }
 
-    cmbType.getSelectionModel().select(0);//Defaults number to 1
+    cmbType.getSelectionModel().select(0);
+    //Defaults number to 1
   }
 
-  public void connectToDb()
-  {
+  /**
+   * This method initializes the program to connect to database and populate it with data.
+   */
+  public void connectToDb() {
 
-    final String JDBC_DRIVER = "org.h2.Driver";
-    final String DB_URL = "jdbc:h2:./res/HR";
+    final String Jdbc_Driver = "org.h2.Driver";
+    final String Db_Url = "jdbc:h2:./res/HR";
 
     //  Database credentials
-    final String USER = "";
-    final String PASS = "";
+    final String User = "";
+    final String Pass = "";
     Connection conn = null;
     Statement stmt = null;
     System.out.println("Product added");
-    String product = productName.getText();//used to get product name from text box
-    System.out.println(product);// used to print out product
-    String manufacturer = manufactureName.getText();//used to get manufacturer name from textbox
-    System.out.println(manufacturer);//used to print out manufacturer
+    String product = productName.getText();
+    //used to get product name from text box
+    System.out.println(product);
+    // used to print out product
+    String manufacturer = manufactureName.getText();
+    //used to get manufacturer name from textbox
+    System.out.println(manufacturer);
+    //used to print out manufacturer
     try {
       // STEP 1: Register JDBC driver
-      Class.forName(JDBC_DRIVER);
+      Class.forName(Jdbc_Driver);
 
       //STEP 2: Open a connection
-      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+      conn = DriverManager.getConnection(Db_Url, User, Pass);
 
       //STEP 3: Execute a query
       stmt = conn.createStatement();
 
-      String insertSql =  "INSERT INTO Product(type, manufacturer, name) "
-          + "VALUES ( 'AUDIO', 'Apple', 'iPod' )"; //sql statement used to add into product table
+      String insertSql = "INSERT INTO Product(type, manufacturer, name) "
+          + "VALUES ( 'AUDIO', 'Apple', 'iPod' )";
+      //sql statement used to add into product table
 
-
-      stmt.executeUpdate(insertSql);//Used to execute sql statement.
-
-
-
-
+      stmt.executeUpdate(insertSql);
+      //Used to execute sql statement.
 
       // STEP 4: Clean-up environment
       stmt.close();
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,18 @@
+//Programmer Name:Brandon Santiago
+//This java file is the main file for the program. Doesn't contain much code
+//since the coding goes into Controller mostly.
+//Date:9/19/2020
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application{
-
-  public static void main(String[]args){
+public class Main extends Application {
+  public static void main(String[]args) {
     launch(args);
   }
+
   @Override
   public void start(Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
